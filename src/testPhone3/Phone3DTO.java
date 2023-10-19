d4adb9e6a2a6319a9f9c0805f2d2ef0f3d985a97
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-10-19 17:09:26 +0900

    .

--- src/testPhone3/Phone3DTO.java
+++ src/testPhone3/Phone3DTO.java
@@ -4,12 +4,16 @@ import testPhone2.Phone2DTO;
 
 public class Phone3DTO extends Phone2DTO {
 
+
 	public String internetSpeed;
+	public int channel;
+	public int onf;
 	
 	
 	public Phone3DTO (String color, String model, String condition, String internetSpeed) {
 		super(color, model, condition);
 		this.internetSpeed = internetSpeed;
+		
 	}
 	
 	
