6eb27f1f05ca74a4154690788d9d6e500044df79
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2023-10-19 19:08:46 +0900

    Update Phone2DAO.java

--- src/testPhone2/Phone2DAO.java
+++ src/testPhone2/Phone2DAO.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 import testPhone1.Phone1DAO;
 
-public class Phone2DAO extends Phone1DAO{
+public class Phone2DAO extends Phone1DAO {
 	Scanner sc = new Scanner(System.in);
 	Phone2DTO dto = new Phone2DTO();
-	
+	boolean check;
+
 	public void init(Phone2DTO dto) {
 		this.dto = dto;
 	}
@@ -17,91 +18,86 @@ public class Phone2DAO extends Phone1DAO{
 		Phone2DTO dto = new Phone2DTO();
 		init(dto);
 		Phone();
-		PowerOn(false);
-		System.out.println(dto.isPower);
+		this.check = PowerOn(false);
+		
+		System.out.println(check);
 		mainScreen();
 //		if (dto.button.equals("On")) {
 //			System.out.println("이니셜-2가 켜졌습니다.");
 //			dto.isPower = true;
 //		}
 	}
-	
-	@Override
-	public void PowerOn(boolean isPower) {
-		System.out.println("모델명: "+dto.getModel()+"색상 :"+dto.getColor()+"전원:"+isPower);
-		if(isPower==false) {
-			System.out.println("전원이 켜집니다");
-			dto.isPower =true;
-		}
-	}
+//	
+//	@Override
+//	public void PowerOn(boolean isPower) {
+//		System.out.println("모델명: "+dto.getModel()+"색상 :"+dto.getColor()+"전원:"+isPower);
+//		if(isPower==false) {
+//			System.out.println("전원이 켜집니다");
+//			dto.isPower =true;
+//		}
+//	}
 
 	public void mainScreen() {
-		System.out.println("버튼을 눌러주세요 (dmb - \"dmb\"입력)" );
+		System.out.println("버튼을 눌러주세요 (dmb - \"dmb\"입력)");
 		button();
-		dmbTurn();
 	}
-		
 
-	
 	public void dmbOff() {
-		if(dto.button.equals("off") || dto.button.equals("OFF")) {
+		if (dto.button.equals("off") || dto.button.equals("OFF")) {
 			System.out.println("dmb 를 종료합니다");
 		}
 	}
-	
+
 	public void button() {
-		dto.button = sc.nextLine();
-		if(dto.button.equals("off")) {
-			System.out.println("전화기가 꺼집니다.");
-			PowerOff(true);
-		}else if(dto.button.equals("dmb")) {
-			dmbTurn();
+		while (check) {
+			dto.button = sc.nextLine();
+			if (dto.button.equals("전원끄기")) {
+				this.check = PowerOff(true);
+			} else if (dto.button.equals("dmb")) {
+				dmbTurn();
+			}
 		}
 	}
 
-
 	public void dmbTurn() {
-		while(dto.isPower) {
-			if(dto.button.equals("dmb") || dto.button.equals("DMB")) {
+		while (check) {
+			if (dto.button.equals("dmb") || dto.button.equals("DMB")) {
 				System.out.println("DMB가 켜집니다. 현재 채널 : 정보없음");
 				dto.channel = "정보없음";
 				dmbOff();
 				dmbChannel();
 				break;
-			}else {
+			} else {
 				System.out.println("유효한 값이 아닙니다.");
 				button();
 				continue;
 			}
 		}
 	}
+
+	
 	
 	
 	public void dmbChannel() {
 		String temp;
 		System.out.println("DMB 채널을 입력해주세요");
-		while(dto.isPower) {
+		while (check) {
 			try {
-				 button();
-				if(dto.button.equals("off") || dto.button.equals("OFF")) {
+				button();
+				if (dto.button.equals("off") || dto.button.equals("OFF")) {
 					System.out.println("DMB가 종료됩니다.");
 					mainScreen();
 					break;
 				}
 				temp = dto.button;
-			}catch (NumberFormatException e) {
+			} catch (NumberFormatException e) {
 				System.out.println("숫자를 입력해주세요");
 				continue;
 			}
 			System.out.println("DMB방송의 채널을 변경합니다");
 			System.out.println(dto.channel + "=>" + dto.button + "로 채널을 변경합니다 ");
 			dto.channel = dto.button;
-			
-		}
+
 		}
 	}
-	
-	
-	
-	
-
+}
